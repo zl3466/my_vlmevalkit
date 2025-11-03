c50fa684de2be40d83690cca3c6783c09b92d883
zl3466 <zl3466@nyu.edu>
2025-11-02 23:30:02 -0500

    adding system prompt to add thinking mode

--- nv_script/eval_qwen_tuned.sh
+++ nv_script/eval_qwen_tuned.sh
@@ -59,7 +59,7 @@ source /lustre/fsw/portfolios/nvr/users/ymingli/miniconda3/etc/profile.d/conda.s
 conda activate allanb
 export HF_HUB_CACHE="/lustre/fsw/portfolios/nvr/users/ymingli/cache/huggingface/hub"
 #export OPENAI_API_KEY="$OPENAI_API_KEY"
-export THOUGHT_PROCESS=1
+export SPLIT_THINK=True
 export MMEVAL_ROOT="./outputs/$mode"
 
 cd "/lustre/fsw/portfolios/nvr/users/ymingli/projects/playground/github/all_angles_bench/VLMEvalkit"
